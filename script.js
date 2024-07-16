5721a123f4d55697683e6534aa6e2c327294e841
Dave the bear <davebear@betterlifepartners.com>
2024-07-15 22:30:51 -0400

    sorry this one is low effort, not a lot of time

--- script.js
+++ script.js
@@ -1,29 +1,22 @@
-const button = document.getElementById('pick-button');
-const result = document.getElementById('result');
+const buttons = document.querySelectorAll('button');
+const storyText = document.getElementById('story');
+const body = document.getElementById('body');
 
-button.addEventListener('click', () => {
-  const colors = ['blue', 'blue'];
-  const randomIndex = Math.floor(Math.random() * colors.length);
-  const chosenColor = colors[randomIndex];
 
-  result.textContent = `You picked ${chosenColor}`;
-  result.style.color = chosenColor;
-  playSound('https://ia903104.us.archive.org/34/items/Coldplay_201807/Yellow.mp3');
-  var body = document.getElementsByTagName('body');
-  document.body.classList.add('it-was-all-yellow');
 
-  setTimeout(function () {
-		const container = document.getElementById('container');
-		container.style.display = 'none';
-		const myImage = new Image(480, 208);
-		myImage.src = "https://pa1.aminoapps.com/7805/11426e762c47482c08c924edd389503c0e520607r1-480-208_hq.gif";
-		document.body.appendChild(myImage);
-    }, 8400);
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+
+
+    if (button.id === 'button1') {
+      storyText.textContent = "You wander deeper into the forest. The air grows colder, and the greyness thickens. Suddenly, a giant blue butterfly lands on your shoulder. Team Blue.";
+    } else if(button.id ==="button2") {
+      storyText.textContent = "You follow the faint blue light, which leads you to a shimmering blue lake. A lone blue flower grows on the bank. Team Blue.";
+    }
+    body.style.backgroundColor='blue';
+    body.style.color='white';
+
+    buttons[0].style.display='none';
+    buttons[1].style.display='none';
+  });
 });
-
-function playSound(url) {
-    var a = new Audio(url);
-    a.play();
-}
-
-
